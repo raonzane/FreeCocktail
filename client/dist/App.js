47d233fb37e2567af8719f04d7374de8bd215df5
chayezo <yejinchaa@naver.com>
2022-01-14 10:40:27 +0900

    [Client/settings] add : 초기 환경 세팅

---
+++ client/dist/App.js
@@ -0,0 +1,7 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import './App.css';
+function App() {
+    return (_jsx("div", Object.assign({ className: "App" }, { children: _jsx("h1", { children: "\uBA54\uB871" }, void 0) }), void 0));
+}
+export default App;
+//# sourceMappingURL=App.js.map
