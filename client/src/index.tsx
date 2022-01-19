2259d9ff530c3311d09ae23060c4dd02d2994f82
95mg <intheroom82@naver.com>
2022-01-19 15:45:01 +0900

    [Client] Add: merge commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,13 +8,9 @@ import { store } from './_store/store';
 
 ReactDOM.render(
   <Provider store={store}>
-<<<<<<< HEAD
     <BrowserRouter>
       <App />
     </BrowserRouter>
-=======
-    <App />
->>>>>>> 185c8b3... add : 초기 환경 세팅
   </Provider>,
   document.getElementById('root')
 );
