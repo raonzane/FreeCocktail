bf399d5f11033556cef0290331bc6d9e5b9af354
raonzane <raonzane@gmail.com>
2022-01-24 13:10:48 +0900

    test

--- client/src/Components/RecipeCard.tsx
+++ client/src/Components/RecipeCard.tsx
@@ -1,1 +1,77 @@
-export {};
+import React from 'react';
+import styled from 'styled-components';
+
+const RecipeCard = function RecipeCard() {
+  return (
+    <div>
+      <Card>
+        <CardImg src="/개구리.png" />
+        <hr />
+        <CardDetails>
+          <CardName>Cocktail Name</CardName>
+          <CardLikes>
+            <LikesImg src="/Like.png" />
+            <LikesNum>Count</LikesNum>
+          </CardLikes>
+        </CardDetails>
+        <CardTags>
+          <CardTag>#Tag1</CardTag>
+          <CardTag>#Tag2</CardTag>
+          <CardTag>#Tag3</CardTag>
+        </CardTags>
+      </Card>
+    </div>
+  );
+};
+
+const Card = styled.div`
+  border: 1px solid #000;
+  border-radius: 0.3rem;
+  padding: 0.5rem;
+`;
+
+const CardImg = styled.img`
+  width: 12rem;
+  height: 14rem;
+  cursor: pointer;
+`;
+
+const CardDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 0.5rem;
+`;
+
+const CardName = styled.div`
+  font-size: 1.2rem;
+`;
+
+const CardLikes = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  gap: 0.15rem;
+`;
+
+const LikesImg = styled.img`
+  width: 1rem;
+  height: 1rem;
+`;
+const LikesNum = styled.div`
+  height: 1rem;
+  color: #000;
+`;
+
+const CardTags = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+const CardTag = styled.div`
+  border: 1px solid #000;
+  border-radius: 0.3rem;
+  padding: 0.1rem;
+`;
+
+export default RecipeCard;
--- client/src/Pages/RecipeList.tsx
+++ client/src/Pages/RecipeList.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import RecipeCard from 'Components/RecipeCard';
 
 const RecipeList = function RecipeList() {
-  return <div>RecipeList</div>;
+  return (
+    <div>
+      <div> RecipeList</div>
+      <CardArea>
+        <RecipeCard />
+      </CardArea>
+    </div>
+  );
 };
+
+const CardArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20%;
+`;
 
 export default RecipeList;
