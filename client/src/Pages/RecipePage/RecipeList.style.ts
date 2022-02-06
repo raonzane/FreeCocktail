c1309c2c8c660d43d6d0f97d435ee99314896550
95mg <intheroom82@naver.com>
2022-02-06 16:41:00 +0900

    Rename: #12 레시피 페이지 폴더 생성 및 style.ts파일 생성

---
+++ client/src/Pages/RecipePage/RecipeList.style.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Item,
+  Label,
+  Menu,
+  Segment,
+  Step,
+  Table,
+} from 'semantic-ui-react';
