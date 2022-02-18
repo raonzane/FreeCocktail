1fd30c3c1a8c7a946195fc939389f7ae4f5311ab
95mg <intheroom82@naver.com>
2022-02-18 14:41:11 +0900

    Design: social login button css

--- client/src/Pages/RecipePage/RecipeList.style.ts
+++ client/src/Pages/RecipePage/RecipeList.style.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
   Button,
   Container,
@@ -13,3 +14,24 @@ import {
   Step,
   Table,
 } from 'semantic-ui-react';
+import { theme } from '../../styles/theme';
+
+const style = {
+  h1: {
+    marginTop: '3em',
+  },
+  h2: {
+    margin: '4em 0em 2em',
+  },
+  h3: {
+    marginTop: '2em',
+    padding: '2em 0em',
+  },
+  last: {
+    marginBottom: '300px',
+  },
+};
+
+export const FilterButton = styled.button`
+  background-color: #cb77ff;
+`;
--- client/src/Pages/RecipePage/RecipeList.tsx
+++ client/src/Pages/RecipePage/RecipeList.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FilterButton } from './RecipeList.style';
 
 const RecipeList = function RecipeList() {
-  return <div>RecipeList</div>;
+  return <FilterButton>generation kill</FilterButton>;
 };
 
 export default RecipeList;
