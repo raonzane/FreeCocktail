5c5ebee9fefc5e38f2badfa2c7bdf368dec5d52d
95mg <intheroom82@naver.com>
2022-03-29 22:18:54 +0900

    Feature: 구글 로그인 userSlice, persist 연결 완료

--- client/src/Components/SocialLoginGoogle.tsx
+++ client/src/Components/SocialLoginGoogle.tsx
@@ -1,21 +1,31 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { socialUserAsnyc } from '../_slices/userSlice';
 
 axios.defaults.withCredentials = true;
 
-//! 참조 https://bit.ly/3Gf0IS4
 const SocialLoginGoogle = function SocialLoginGoogle() {
   useEffect(() => {
     googleSDK();
   }, []);
 
+  interface GoogleDispatchArg {
+    snsName: string;
+    accessToken: string;
+  }
+
   const { REACT_APP_GOOGLE_CLIENTID, REACT_APP_REDIRECT_URI } = process.env;
   const { google } = window;
+  const dispatch = useDispatch();
 
   function handleCredentialResponse(response: any) {
-    console.log('ID 토큰 ', response.credential); //* ID 토큰
-    const idToken = response.credential;
-    axios.post('http://localhost:3001/oauth/google', { data: idToken });
+    const accessToken = response.credential;
+    const GOOGLE_DISPATCH_ARG: GoogleDispatchArg = {
+      snsName: 'google',
+      accessToken,
+    };
+    dispatch(socialUserAsnyc(GOOGLE_DISPATCH_ARG));
   }
 
   function googleSDK() {
@@ -25,17 +35,16 @@ const SocialLoginGoogle = function SocialLoginGoogle() {
       callback: handleCredentialResponse,
     });
     google.accounts.id.renderButton(document.getElementById('buttonDiv'), {
+      type: 'icon',
       theme: 'outline',
       size: 'large',
+      text: 'signin_with',
+      shape: 'rectangular',
     });
     google.accounts.id.prompt();
   }
 
-  return (
-    <button type="button" id="buttonDiv">
-      Google Login
-    </button>
-  );
+  return <div id="buttonDiv">Google Login</div>;
 };
 
 export default SocialLoginGoogle;
