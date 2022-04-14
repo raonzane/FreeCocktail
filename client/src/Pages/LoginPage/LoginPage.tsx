1d4879f7845e6df44e686b03ef4ed2722f9055dd
chayezo <yejinchaa@naver.com>
2022-04-14 14:40:34 +0900

    authSlice, 로그인 관련 테스트

--- client/src/Pages/LoginPage/LoginPage.tsx
+++ client/src/Pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Waves from 'Components/Waves';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuth, ILoginData } from '_slices/authSlice';
+import { Link } from 'react-router-dom';
 import Google from '../../Components/SocialLoginGoogle';
 import Naver from '../../Components/SocialLoginNaver';
 import {
@@ -21,11 +24,45 @@ import { store } from '../../_store/store';
 
 const LoginPage = function LoginPage() {
   console.log('로그인 페이지에서 확인한 state', store.getState());
+
+  const dispatch = useDispatch();
+
+  // 스토어 값 가져오기
+  const { isLoggedIn, isInValid } = useSelector(ILoginData);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const setEmailData = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+  const setPasswordData = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+
+  // dispatch로 setAuth에 서버에 로그인 요청
+  const handleLogin = (email: string, password: string) => {
+    dispatch(setAuth({ email, password }));
+  };
+
+  const handleKeyPress = (
+    e: React.KeyboardEvent,
+    email: string,
+    password: string
+  ) => {
+    if (e.key === 'Enter') {
+      handleLogin(email, password);
+    }
+  };
+
   return (
     <>
       <Waves />
       <Body>
         <MainArea>
+          {/* {isLoggedIn ? (
+            <Redirect to="/" />
+          ) : ( */}
           <Container>
             <ImgContainer>이미지 자리</ImgContainer>
             <ContentContainer>
@@ -33,15 +70,30 @@ const LoginPage = function LoginPage() {
                 <Title>로그인</Title>
               </TitleWrapper>
               <InputTitle>이메일</InputTitle>
-              <InputField type="email" />
+              <InputField
+                type="email"
+                onChange={(e) => setEmailData(e)}
+                onKeyPress={(e) => handleKeyPress(e, email, password)}
+              />
               <InputTitle>비밀번호</InputTitle>
-              <InputField type="password" />
+              <InputField
+                type="password"
+                onChange={(e) => setPasswordData(e)}
+                onKeyPress={(e) => handleKeyPress(e, email, password)}
+              />
               <InvalidMessage>
-                <div>이메일과 비밀번호를 확인해 주세요.</div>
+                {isInValid ? (
+                  <div>이메일과 비밀번호를 확인해 주세요.</div>
+                ) : null}
               </InvalidMessage>
-              <LoginBtn>로그인</LoginBtn>
+              <LoginBtn onClick={() => handleLogin(email, password)}>
+                로그인
+              </LoginBtn>
               <SignupBtn>
-                아직 회원이 아니신가요? <p>회원가입 하러가기</p>
+                아직 회원이 아니신가요?
+                <p>
+                  <Link to="/signupPage">회원가입 하러가기</Link>
+                </p>
               </SignupBtn>
               <SocialLoginBtn2>
                 <div> SNS를 이용한 소셜 로그인 </div>
@@ -52,6 +104,7 @@ const LoginPage = function LoginPage() {
               </SocialLoginBtn2>
             </ContentContainer>
           </Container>
+          {/* )} */}
         </MainArea>
       </Body>
     </>
