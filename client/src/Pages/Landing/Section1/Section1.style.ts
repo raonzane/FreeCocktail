d66dd8a23e82643d3caa57f91871f2c0da75a02b
nnswwd <raonzane@gmail.com>
2022-06-07 18:17:41 +0900

    Feat: #18 랜딩페이지 슬라이드 추가

---
+++ client/src/Pages/Landing/Section1/Section1.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { theme } from '../../../styles/theme';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 5%;
+`;
